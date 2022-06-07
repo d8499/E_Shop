d083e16f46830249782ee413fdc22561002e7c6d
Dhruv <dhruv08499@gmail.com>
2022-06-07 20:09:52 +0530

    Table View In Admin Site

--- storeApp/models/product.py
+++ storeApp/models/product.py
@@ -1,13 +1,15 @@
 from distutils.command.upload import upload
 from email.mime import image
 from statistics import mode
-from unicodedata import name
+from unicodedata import category, name
 from django.db import models
+from .category import Category
 
 class Product(models.Model):
     
     name= models.CharField(max_length=50) # Product Name
     price = models.IntegerField(default=0) # Price
+    category = models.ForeignKey(Category , on_delete=models.CASCADE , default=1)
     description = models.CharField(max_length=200 , default='')
     image = models.ImageField(upload_to = 'uploads/products/') # PIL library required
 
